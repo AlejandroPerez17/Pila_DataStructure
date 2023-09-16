82f5ebc3418c1442f0277a6122541a6b728de79d
Alejandro P <133065156+AlejandroPerez17@users.noreply.github.com>
2023-09-16 17:27:14 -0600

    Create ColaCircular.java

---
+++ ColasEstructura/ColaCircular.java
@@ -0,0 +1,58 @@
+package ColasEstructura;
+
+/**
+ *
+ * @author Alee
+ */
+public class ColaCircular {
+    private Nodo frente;
+private Nodo atras;
+private int longitud;
+public ColaCircular() {
+frente = null;
+atras = null;
+longitud = 0;
+}
+public void insertar(int dato) {
+Nodo nuevoNodo = new Nodo(dato);
+if (estaVacia()) {
+frente = nuevoNodo;
+atras = nuevoNodo;
+atras.setSiguiente(frente);
+} else {
+atras.setSiguiente(nuevoNodo);
+atras = nuevoNodo;
+atras.setSiguiente(frente);
+}
+longitud++;
+}
+public void eliminar() {
+if (!estaVacia()) {
+if (frente == atras) {
+frente = null;
+atras = null;
+} else {
+frente = frente.getSiguiente();
+atras.setSiguiente(frente);
+}
+longitud--;
+}
+}
+public boolean estaVacia() {
+return frente == null;
+}
+public Nodo getFrente() {
+ if (estaVacia()) {
+         System.out.println("Error: Intento de obtener el frente de una cola vacía.");
+     return null;
+       }
+        return frente;
+    }
+
+public Nodo getAtras() {
+return atras;
+}
+public int getLongitud() {
+return longitud;
+}
+}
