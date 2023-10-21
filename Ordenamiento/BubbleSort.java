03108c451ed766f84c7171396c70205a64d28f2a
Alejandro P <133065156+AlejandroPerez17@users.noreply.github.com>
2023-10-20 23:48:23 -0600

    Update BubbleSort.java

--- Ordenamiento/BubbleSort.java
+++ Ordenamiento/BubbleSort.java
@@ -1,1 +1,30 @@
+import java.util.Arrays;
+/**
+ *
+ * @author Alee
+ */
 
+
+public class BubbleSort {
+    public static int[] bubbleSort(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    
+                    System.out.println(Arrays.toString(arr));
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
